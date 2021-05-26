8daee78e073911968e01f923714f05e9a27c9e06
QuangTung97 <quangtung29121997@gmail.com>
2021-05-26 16:27:18 +0700

    Add override.go existed

--- cmd/eventx-update/main.go
+++ cmd/eventx-update/main.go
@@ -14,7 +14,13 @@ const modName = "github.com/QuangTung97/eventx"
 
 var goRegexp = regexp.MustCompile(`^.+\.go$`)
 
-var excluded = []string{"tools.go", "override.go"}
+const (
+	toolsFile    = "tools.go"
+	overrideFile = "override.go"
+)
+
+var excluded = []string{toolsFile, overrideFile}
+var initExcluded = []string{toolsFile}
 
 func stringIn(s string, list []string) bool {
 	for _, e := range list {
@@ -25,7 +31,16 @@ func stringIn(s string, list []string) bool {
 	return false
 }
 
-func makeDirOrRemoveFiles() {
+func fileToBeCopied(filename string, overrideExisted bool) bool {
+	if overrideExisted {
+		return !stringIn(filename, excluded)
+	}
+	return !stringIn(filename, initExcluded)
+}
+
+func makeDirOrRemoveFiles() bool {
+	overrideExisted := false
+
 	err := os.MkdirAll("eventx", os.ModePerm)
 	if err != nil {
 		panic(err)
@@ -33,19 +48,26 @@ func makeDirOrRemoveFiles() {
 
 	destInfo, err := ioutil.ReadDir("eventx")
 	for _, f := range destInfo {
-		if !f.IsDir() && f.Name() != "override.go" {
+		if f.IsDir() {
+			continue
+		}
+
+		if f.Name() != "override.go" {
 			file := path.Join("eventx", f.Name())
 			fmt.Println("Delete:", file)
 			err := os.Remove(file)
 			if err != nil {
 				panic(err)
 			}
+		} else {
+			overrideExisted = true
 		}
 	}
+	return overrideExisted
 }
 
 func main() {
-	makeDirOrRemoveFiles()
+	overrideExisted := makeDirOrRemoveFiles()
 
 	bytes, err := exec.Command("go", "list", "-m", "-f", "{{.Dir}}", modName).Output()
 	if err != nil {
@@ -58,8 +80,8 @@ func main() {
 	}
 	for _, f := range infos {
 		name := f.Name()
-		if goRegexp.MatchString(name) && !stringIn(name, excluded) && !f.IsDir() {
-			fmt.Println("Copy:", f.Name())
+		if goRegexp.MatchString(name) && fileToBeCopied(name, overrideExisted) && !f.IsDir() {
+			fmt.Println("Copy:", name)
 			err := exec.Command("cp", path.Join(dir, name), "eventx").Run()
 			if err != nil {
 				panic(err)
